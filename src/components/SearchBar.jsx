ce59aec52c161b1256fb9ae8434f86b6742cbaf1
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-04-05 16:51:27 -0300

    Adicionado componentes CSS aos estilos globais

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const SearchBar = () => {
+
+    const categorias = [
+        "Natureza",
+        "Pessoas",
+        "Tecnologia",
+        "Animais",
+        "Esportes",
+    ]
+
   return (
-    <div>SearchBar</div>
+    <div className='search-bar'>
+        <input type="text" placeholder='Pesquisar fotos...' />
+        <button>Pesquisar</button>
+        <select>
+            {categorias.map((categoria) => (
+                <option key={categoria} value={categoria}>{categoria}</option>
+            ))}
+        </select>
+    </div>
   )
 }
 
