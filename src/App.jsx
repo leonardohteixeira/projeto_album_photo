fd54ed82352b200883d0fab65d264a9e56e47e1d
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-04-05 15:38:28 -0300

    Configurado acesso a API de fotos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-
 import FotoAmpliada from './components/FotoAmpliada'
 import FotoList from './components/FotoList'
 import SearchBar from './components/SearchBar'
 
 import { useState, useEffect } from 'react'
 
+import axios from 'axios'
+
 function App() {
 
+  const [query, setQuery] = useState("")
+  const [categoria, setCategoria] = useState("")
+
+  const fetchData = async ({query, categoria}) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY
+    const response = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: apiKey
+      }
+    })
+
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchData(query, categoria)
+  }, [])
 
   return (
     <div className='container'>
